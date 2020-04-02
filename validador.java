add92a529f08e84482a2000eb5c247bd5db25d23
MarcosRivitti <marcosrivitti77@gmail.com>
2020-04-02 18:13:50 -0300

    Update validador.java

--- validador.java
+++ validador.java
@@ -47,7 +47,6 @@ class ArchivoContraseniasInvalidas{
 }
 class ReguladorDeContrasenias{
 	static String listaCararcteresValidos = " !\",#$%&()*+-,-./0123456789:;<=>ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`@abcdefghijklmnopqrstuvwxyz{}";
-	//static char [] listaDeCaracteresValidos = {'!','\"',',','#','$','%','&','(',')','*','+','-',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','@','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','}'};
 	static public boolean longitudValida(String contrasenia){
 		return contrasenia.length() >= 8;
 	}
